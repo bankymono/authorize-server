aadd115cf7ebcbad6c8bd45abde8316891583ddb
Ayodeji Bankole <bankymono@gmail.com>
2024-10-20 14:45:10 +0100

    private key auth

--- src/main/java/com/bankymono/authorization_server/config/AuthServerConfig.java
+++ src/main/java/com/bankymono/authorization_server/config/AuthServerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
@@ -14,6 +15,7 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 import javax.sql.DataSource;
 
@@ -21,6 +23,17 @@ import javax.sql.DataSource;
 @Configuration
 @EnableAuthorizationServer
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Value("${password}")
+    private String password;
+
+    @Value("${privateKey}")
+    private String privateKey;
+
+    @Value("${alias}")
+    private String alias;
+
+
     @Autowired
     private AuthenticationManager authenticationManager;
 
@@ -46,7 +59,16 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     @Bean
     public JwtAccessTokenConverter jwtAccessTokenConverter() {
         var converter = new JwtAccessTokenConverter();
-        converter.setSigningKey(jwtKey);
+//        converter.setSigningKey(jwtKey);
+
+        KeyStoreKeyFactory keyStoreKeyFactory
+                = new KeyStoreKeyFactory(
+                        new ClassPathResource(privateKey),
+                password.toCharArray()
+        );
+        converter.setKeyPair(
+                keyStoreKeyFactory.getKeyPair(alias)
+        );
         return converter;
     }
 
